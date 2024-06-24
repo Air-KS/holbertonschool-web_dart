421a0948847141e1914f00d9af2d8a032b2efd90
Air-KS <kevinrogeret@gmail.com>
2024-06-24 14:04:22 +0200

    Task test

--- 0x00-hello-dart/3-print_double.dart
+++ 0x00-hello-dart/3-print_double.dart
@@ -1,5 +1,5 @@
 void main() {
   var n = 3.14159265359;
-  String polarity = n.toStringAsFixed(2);
-    print("Double: $polarity\n");
+  String inDouble = n.toStringAsFixed(2);
+    print("Double: $inDouble\n");
 }
