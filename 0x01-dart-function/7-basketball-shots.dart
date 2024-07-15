b1437f6f3e7f8000ac2ec06bdadd7b1e827de5d1
Air-KS <kevinrogeret@gmail.com>
2024-07-15 13:02:05 +0200

    Basketball Shots

--- 0x01-dart-function/7-basketball-shots.dart
+++ 0x01-dart-function/7-basketball-shots.dart
@@ -1,9 +1,3 @@
-int getPoints(Map<String, int> team) {
-  return (team['Free throws'] ?? 0) * 1 +
-         (team['2 pointer'] ?? 0) * 2 +
-         (team['3 pointer'] ?? 0) * 3;
-}
-
 int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   int teamAPoints = getPoints(teamA);
   int teamBPoints = getPoints(teamB);
@@ -15,4 +9,10 @@ int whoWins(Map<String, int> teamA, Map<String, int> teamB) {
   } else {
     return 0;
   }
+}
+
+int getPoints(Map<String, int> team) {
+  return (team['Free throws'] ?? 0) * 1 +
+         (team['2 pointer'] ?? 0) * 2 +
+         (team['3 pointer'] ?? 0) * 3;
 }
