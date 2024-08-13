70966d4e0812ed89967f7ad7c8cded97b73e810b
Air-KS <kevinrogeret@gmail.com>
2024-08-13 14:54:04 +0200

    rick and morty

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -1,14 +1,27 @@
-import "package:http/http.dart" as http;
+import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-printRmCharacters() async {
+Future<void> printRmCharacters() async {
   try {
-    final res = await http.get(
-      Uri.parse('https://rickandmortyapi.com/api/character'),
+    // Fetch data from the Rick and Morty API
+    final response = await http.get(
+      Uri.parse("https://rickandmortyapi.com/api/character"),
     );
-    var json = jsonDecode(res.body);
-    for (var idx = 0; idx < json['results'].length; idx++) {
-      print("${json['results'][idx]['name']}");
+
+    // Decode the JSON response
+    var jsonResponse = jsonDecode(response.body);
+
+    // Check if the JSON response contains the 'results' key
+    if (jsonResponse['results'] != null) {
+      // Get the list of characters
+      var characters = jsonResponse['results'] as List;
+
+      // Iterate through the list and print each character's name
+      for (var character in characters) {
+        print(character['name']);
+      }
+    } else {
+      print('No results found');
     }
   } catch (err) {
     print('error caught: $err');
